4d4ced74fa31a4e084eb7906dfb6a9181885b8bc
jonathanFS14 <jona08p8@stud.kea.dk>
2022-11-03 12:36:04 +0100

    1

---
+++ src/main/java/org/example/Covid19Data.java
@@ -0,0 +1,69 @@
+package org.example;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Covid19Data {
+
+    private String region;
+    private String aldersgruppe;
+    private int bekfræftedeTilfælde;
+    private int døde;
+    private int indlagtePåIntensivAfdeling;
+    private int indlagte;
+    private String dato;
+
+    public Covid19Data(String region, String aldersgruppe, int bekfræftedeTilfælde, int døde, int indlagtePåIntensivAfdeling, int indlagte, String dato){
+    this.region = region;
+    this.aldersgruppe = aldersgruppe;
+    this.bekfræftedeTilfælde = bekfræftedeTilfælde;
+    this.døde = døde;
+    this.indlagtePåIntensivAfdeling = indlagtePåIntensivAfdeling;
+    this.indlagte = indlagte;
+    this.dato = dato;
+    }
+
+    @Override
+    public String toString() {
+        return "Covid19Data: " + region + " " + aldersgruppe + " " + bekfræftedeTilfælde + " " + døde + " " + indlagtePåIntensivAfdeling + " " + indlagte + " " + dato;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Covid19Data> arrayList = new ArrayList<>();
+
+        File f = new File("C:\\Users\\jonat\\OneDrive\\Skrivebord\\noegletal_pr_region_pr_aldersgruppe.csv");
+        try {
+            Scanner sc = new Scanner(f);
+            while (sc.hasNextLine()) {
+                // læs linje
+                String linje = sc.nextLine();
+                String[] attributter = linje.split(";");
+                Covid19Data indlaestdata = new Covid19Data(
+                attributter[0],
+                attributter[1],
+                Integer.parseInt(attributter[2]),
+                Integer.parseInt(attributter[3]),
+                Integer.parseInt(attributter[4]),
+                Integer.parseInt(attributter[5]),
+                attributter[6]
+                );
+                arrayList.add(indlaestdata);
+            }
+            sc.close();
+
+        } catch (IOException e) {
+            System.out.println("Fejl i kommunikation med fil: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Fejl i filformat: " + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e){
+            //System.out.println(e);
+        }
+
+        for (Covid19Data c: arrayList){
+            System.out.println(c);
+        }
+    }
+
+}
